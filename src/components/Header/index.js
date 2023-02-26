461ad78d3974986c783713b0ea404122a697e67b
Ursu Adrian <uursu.adrian@gmail.com>
2023-02-26 13:02:40 +0100

    created Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Header = () => {
+  return (
+    <Head>
+      <title>YourPhotos</title>
+      <meta
+        name="description"
+        content="Explore a collection of stunning photographs that showcase the beauty of nature, people, and everyday life. Our photo portfolio features a diverse range of styles and subjects, from candid street photography to breathtaking landscapes and portraits. Each photo is carefully composed and edited to convey a unique story and evoke powerful emotions. Browse our gallery and discover the artistry and creativity behind each image."
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
+import Header from "../components/Header";
+
 export default function Home() {
   return (
     <>
+      <Header />
       <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
     </>
   );
