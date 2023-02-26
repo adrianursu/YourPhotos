41ee96aeae13fedca0b393ac7463cb18e644e91e
Ursu Adrian <uursu.adrian@gmail.com>
2023-02-26 14:58:15 +0100

    updated MainLayout

--- src/components/Layout/MainLayout/index.js
+++ src/components/Layout/MainLayout/index.js
@@ -1,6 +1,6 @@
 const MainLayout = (props) => {
   return (
-    <main className="px-10 bg-gray-900 bg-none md:px-20 lg:px-20">
+    <main className="px-10 bg-gray-900 bg-none md:px-14 lg:px-16">
       {props.children}
     </main>
   );
