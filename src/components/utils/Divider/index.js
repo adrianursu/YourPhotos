306f68a708d8a2779aeb372b5b45915cd9cb3bd3
Ursu Adrian <uursu.adrian@gmail.com>
2023-02-27 21:22:12 +0100

    fixed typo

--- src/components/utils/Divider/index.js
+++ src/components/utils/Divider/index.js
@@ -1,7 +1,7 @@
 const Divider = () => {
   return (
     <hr
-      class="w-full h-px mx-auto border-0 rounded  "
+      className="w-full h-1 mx-auto border-0 rounded"
       style={{ background: "rgb(31,41,55)" }}
     />
   );
